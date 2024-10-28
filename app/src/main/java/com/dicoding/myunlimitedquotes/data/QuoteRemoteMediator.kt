fed6dab0905b81f9cba0c7e5494a21c04db14ee7
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-10-28 14:16:19 +0800

    RemoteKey

--- app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRemoteMediator.kt
+++ app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRemoteMediator.kt
@@ -4,6 +4,7 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
 import com.dicoding.myunlimitedquotes.database.QuoteDatabase
 import com.dicoding.myunlimitedquotes.network.ApiService
 import com.dicoding.myunlimitedquotes.network.QuoteResponseItem
@@ -13,11 +14,31 @@ class QuoteRemoteMediator(
     private val database: QuoteDatabase,
     private val apiService: ApiService
 ) : RemoteMediator<Int, QuoteResponseItem>() {
+
+    private companion object {
+        const val INITIAL_PAGE_INDEX = 1
+    }
+    override suspend fun initialize(): InitializeAction {
+        return InitializeAction.LAUNCH_INITIAL_REFRESH
+    }
+
     override suspend fun load(
         loadType: LoadType,
         state: PagingState<Int, QuoteResponseItem>
     ): MediatorResult {
-        TODO("Not yet implemented")
+        val page = INITIAL_PAGE_INDEX
+        try {
+            val responseData = apiService.getQuote(page, state.config.pageSize)
+            val endOfPaginationReached = responseData.isEmpty()
+            database.withTransaction {
+                if (loadType == LoadType.REFRESH) {
+                    database.quoteDao().deleteAll()
+                }
+                database.quoteDao().insertQuote(responseData)
+            }
+            return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
+        } catch (exception: Exception) {
+            return MediatorResult.Error(exception)
+        }
     }
-
 }
